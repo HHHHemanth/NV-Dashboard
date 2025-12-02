fe4d8f7662f495f92486b034249631b7d5aceb31
HHHHemanth <hemanth2102003acc2@gmail.com>
2025-12-03 00:19:16 +0530

    CHANGED URL

--- src/components/parameterGraph.tsx
+++ src/components/parameterGraph.tsx
@@ -87,7 +87,8 @@ export default function ParameterGraph({ onPointSelected, onTokenChange }: {
         days: days,
         type: parameter
       };
-      const resp = await fetch("http://127.0.0.1:8000/api/assetpart/ParameterTrends", {
+      const API_BASE = (process.env.NEXT_PUBLIC_API_BASE || "http://127.0.0.1:8000").replace(/\/$/, "");
+      const resp = await fetch(`${API_BASE}/api/assetpart/ParameterTrends`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
